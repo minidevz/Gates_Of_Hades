e679e0dc2517b708700e04ccefae8d6705edd4d0
minidevz <muhammadibrahimhisham@gmail.com>
2020-10-22 19:04:10 +0500

    some stuff

---
+++ Gates_Of_Hades/Assets/FreeCam.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreeCam : MonoBehaviour
+{
+    public bool isEnabled;
+    public float sensitivityX = 8F;
+    public float sensitivityY = 8F;
+    float mHdg = 0F;
+    float mPitch = 0F;
+   
+  
+    void Update ( )
+    {
+        if(isEnabled)
+        { 
+        float deltaX = Input.GetAxis("Horizontal") * sensitivityX * Time.deltaTime;
+        float deltaY = Input.GetAxis("Vertical") * sensitivityY * Time.deltaTime;
+        transform.Translate(deltaX, deltaY , 0);
+        }
+    }
+
+}
